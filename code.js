65de916a41230eb37148d4af899d249822697cab
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-28 11:42:11 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,27 +2,14 @@ function e(n) {
     if (n === 1) { return 1; }
     else if (n === 2) { return 2; }
     else {
-        return e2(n, 1, factorial(2));
+        return e2(n, 1, 2);
     }
 }
 
-function e2(n, count, total, fact = 1) {
+function eHelp(n, count, total, fact = 1) {
     if (count == n) { return total; }
     else {
         fact = fact * (count + 1);
-        return e2(n, count + 1, total + (1.0 / fact), fact);
-    }
-}
-
-function factorial(n) {
-    return factorialAccume(n, 1);
-}
-
-function factorialAccume(a, acc) {
-    if (a == 0) {
-        return acc;
-    }
-    else {
-        return factorialAccume(a - 1, a * acc);
+        return eHelp(n, count + 1, total + (1.0 / fact), fact);
     }
 }
