e9b90336cd2f34788d92125f84a615c76af85eac
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-28 02:05:42 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,3 +4,8 @@ eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+assert(e(1) === 1);
+assert(e(2) === 2);
+assert(e(3) === 2.6666666666666665);
+assert(e(7) === 2.7182539682539684); 
+assert(e(20) === 2.7182818284590455);
